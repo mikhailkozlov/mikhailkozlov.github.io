6680a07c2ec749cad15487ed30a5be2abf6d9cdc
mikhailkozlov <code@315design.com>
2014-06-11 21:22:55 -0700

    Portfolio code

---
+++ components/js/app.js
@@ -0,0 +1,44 @@
+var portfolioControllers = angular.module('portfolioControllers', []);
+
+var myApp = angular.module('myApp', ['ngRoute','portfolioControllers'], function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+});
+/**
+ *
+ * @constructor
+ */
+portfolioControllers.controller('PortfolioListController', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('/components/js/portfolio.json').success(function (data) {
+            $scope.works = data.works;
+        });
+    }]);
+
+portfolioControllers.controller('PortfolioViewController', ['$scope', '$routeParams','$http',
+    function ($scope, $routeParams, $http) {
+        $http.get('/components/js/portfolio/'+$routeParams.slug+'.json').success(function (data) {
+            $scope.work = data;
+        });
+    }]
+).config(function($sceProvider) {
+        // Completely disable SCE.  For demonstration purposes only!
+        // Do not use in new projects.
+        $sceProvider.enabled(false);
+    });
+
+myApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: '/components/partials/portfolio.html',
+                controller: 'PortfolioListController'
+            }).
+            when('/:slug', {
+                templateUrl: '/components/partials/portfolio-detail.html',
+                controller: 'PortfolioViewController'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
